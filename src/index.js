d50adaef696c5c1ba61176fbe3249ab1e9ffc496
Amir Rajabi <amir@amirrajabi.com>
2016-09-27 20:38:09 +1000

    Add base setup about react seed

---
+++ src/index.js
@@ -0,0 +1,21 @@
+/* eslint-disable import/default */
+
+import React from 'react';
+import {render} from 'react-dom';
+import { Provider } from 'react-redux';
+import { Router, browserHistory } from 'react-router';
+import routes from './routes';
+import configureStore from './store/configureStore';
+import './styles/styles.scss';
+import { syncHistoryWithStore } from 'react-router-redux';
+
+require('./favicon.ico');
+
+const store = configureStore(),
+  history = syncHistoryWithStore(browserHistory, store);
+
+render(
+  <Provider store={store}>
+    <Router history={history} routes={routes} />
+  </Provider>, document.getElementById('app')
+);
